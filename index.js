a76dd82584e5a552878d550a9111fd7f5c19c2cd
Paul cartau <pcartau@student.42.fr>
2021-07-11 20:10:27 +0200

    save
    Signed-off-by: Paul cartau <pcartau@student.42.fr>

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ const publicRoutes = ['/auth'];
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({limit: '1mb'}));
 app.use(bodyParser.json());
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   if (publicRoutes.includes(req.path)) {
     return next();
   }
